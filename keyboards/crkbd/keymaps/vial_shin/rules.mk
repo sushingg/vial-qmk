ce4e418d20ed9fef2b761292bba73d82341ef2c7
sushingg <t.shingg@gmail.com>
2024-06-26 23:36:11 +0700

    add keymaps

---
+++ keyboards/crkbd/keymaps/vial_shin/rules.mk
@@ -0,0 +1,18 @@
+VIA_ENABLE          = yes
+VIAL_ENABLE         = yes
+LTO_ENABLE          = no
+
+RGBLIGHT_ENABLE     = no
+RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+MOUSEKEY_ENABLE     = yes
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
+EXTRAKEY_ENABLE     = yes          # Audio control and System control
+COMBO_ENABLE        = yes
+
+QMK_SETTINGS        = yes
+
+SERIAL_DRIVER = vendor
+
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
---
+++ keyboards/crkbd/keymaps/vial_shin_miryoku/readme.md
@@ -0,0 +1,1 @@
+[JSON file of the Miryoku from manna-harbour](https://www.reddit.com/r/crkbd/comments/1bxrv2y/corne_keyboard_want_to_use_miryoku_qmk_how_to_add/)
